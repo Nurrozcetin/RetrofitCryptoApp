d027241889b1fb998b9a892d2aedf7763b8dbd26
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-04-20 21:17:48 +0300

    added rxJava, retrofit

---
+++ app/src/main/java/com/example/retrofitcrypto/view/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.retrofitcrypto.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.retrofitcrypto.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
+
+
+
+    }
+}
+
+
+
